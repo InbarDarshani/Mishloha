44ff4a2e2b996460f14467dca7b6d8324ef44fae
InbarDarshani <InbarProjects@gmail.com>
2023-01-26 18:38:11 +0200

    The Main controller added

---
+++ Backend/Mishloha.Api/Controllers/GithubRepoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mishloha.Models;
+using Mishloha.Service;
+
+namespace MishlohaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GithubRepoController : ControllerBase
+    {
+        private readonly GithubRepoService githubRepoService;
+
+        public GithubRepoController()
+        {
+            githubRepoService = new GithubRepoService();
+        }
+
+        // GET api/<GithubRepoController>/1
+        [HttpGet]
+        public IActionResult Get(Enums.Timeframe timeframe, int page)    //TODO: Check enum out of range
+        {
+            try
+            {
+                List<GithubRepo> result = githubRepoService.SearchByTimeframe(timeframe, page);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.ToString());
+            }
+        }
+    }
+}
